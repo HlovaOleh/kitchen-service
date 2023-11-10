c6d0355200d50bf82fcd7c65a1d2fdf2ffb9dd6d
Hlova Oleh <hlovaoleh0@gmail.com>
2023-11-10 22:44:08 +0200

    create models.py with DishType, Cook(AbstractUser), Dish models. Customize django admin page.

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from restaurant.models import DishType, Dish, Cook
+
+
+@admin.register(DishType)
+class DishTypeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Dish)
+class DishAdmin(admin.ModelAdmin):
+    list_display = ["name", "description", "price", "dish_type"]
+    list_filter = ["dish_type__name", ]
+    search_fields = ["name", ]
+
+
+@admin.register(Cook)
+class CookAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("years_of_experience",)
+    fieldsets = UserAdmin.fieldsets + (("Additional info", {"fields": ("years_of_experience",)}),)
+    add_fieldsets = UserAdmin.add_fieldsets + (("Additional info", {"fields": ("years_of_experience",)}),)
--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,28 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class DishType(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Cook(AbstractUser):
+    years_of_experience = models.IntegerField(null=True)
+
+    class Meta:
+        verbose_name = "Cook"
+        verbose_name_plural = "Cooks"
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField()
+    price = models.DecimalField(decimal_places=2, max_digits=5)
+    dish_type = models.ForeignKey(
+        DishType, on_delete=models.CASCADE, related_name="dish"
+    )
+    cooks = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="dishes")
