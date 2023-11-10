20658b641dd5847a18e802d70ec66d4643b5f0a2
Hlova Oleh <hlovaoleh0@gmail.com>
2023-11-10 22:56:35 +0200

    create main page and sidebar

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,3 +1,16 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from restaurant.models import Cook, DishType, Dish
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    num_cooks = Cook.objects.count()
+    num_dish_types = DishType.objects.count()
+    num_dish = Dish.objects.count()
+    context = {
+        "num_cooks": num_cooks,
+        "num_dish_types": num_dish_types,
+        "num_dish": num_dish
+    }
+    return render(request, "restaurant/index.html", context=context)
