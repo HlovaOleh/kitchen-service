4e3ccdc07d9810b8fcd86b52aa5861ef7d607344
Hlova Oleh <hlovaoleh0@gmail.com>
2023-11-10 23:35:34 +0200

    create cook list, cook detail, dish list, dish detail, dishtype list pages. Also create views and urls for all this pages and add pagination to pages.

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,9 +1,29 @@
 from django.urls import path
 
-from .views import index
+from .views import (
+    index,
+    DishTypeListView,
+    DishListView,
+    DishDetailView,
+    CookListView,
+    CookDetailView
+)
 
 urlpatterns = [
-    path("", index, name="index")
+    path("", index, name="index"),
+    path(
+        "dish_types/",
+        DishTypeListView.as_view(),
+        name="dish_type-list"
+    ),
+    path("dishes/", DishListView.as_view(), name="dish-list"),
+    path("dish/<int:pk>/", DishDetailView.as_view(), name="dish-detail"),
+    path("cooks/", CookListView.as_view(), name="cook-list"),
+    path(
+        "cooks/<int:pk>/",
+        CookDetailView.as_view(),
+        name="cook-detail"
+    ),
 ]
 
 app_name = "restaurant"
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,16 +1,47 @@
+from typing import Any
+
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.views import generic
 
 from restaurant.models import Cook, DishType, Dish
 
 
 def index(request: HttpRequest) -> HttpResponse:
-    num_cooks = Cook.objects.count()
-    num_dish_types = DishType.objects.count()
-    num_dish = Dish.objects.count()
     context = {
-        "num_cooks": num_cooks,
-        "num_dish_types": num_dish_types,
-        "num_dish": num_dish
+        "num_cooks": Cook.objects.count(),
+        "num_dish_types": DishType.objects.count(),
+        "num_dish": Dish.objects.count()
     }
     return render(request, "restaurant/index.html", context=context)
+
+
+class DishTypeListView(generic.ListView):
+    model = DishType
+    queryset = DishType.objects.all()
+    paginate_by = 5
+
+
+class DishListView(generic.ListView):
+    model = Dish
+    queryset = Dish.objects.select_related("dish_type")
+    paginate_by = 5
+
+
+class DishDetailView(generic.DetailView):
+    model = Dish
+
+
+class CookListView(generic.ListView):
+    model = Cook
+    paginate_by = 5
+
+
+class CookDetailView(generic.DetailView):
+    model = Cook
+    queryset = Cook.objects.all()
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context["dish_list"] = self.object.cooks.select_related("dishes")
+        return context
